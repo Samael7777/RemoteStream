64a36d259521a29b0c4be54163826c4d4e6fadef
Вадим Кутьин <vadim.kutyn@gmail.com>
2024-12-27 14:19:28 +0100

    Add ssl sertificate support

--- RemoteStream.Server/RemoteStreamServer.cs
+++ RemoteStream.Server/RemoteStreamServer.cs
@@ -8,18 +8,36 @@ public class RemoteStreamServer : IDisposable
 {
     private readonly Grpc.Core.Server _server;
     private readonly RemoteStreamServerRpcImpl _rpcService;
+
+    public event EventHandler<Exception>? ExceptionThrew;
+    public event EventHandler? StreamClosed;
+    
     public RemoteStreamServer(Stream stream, IPEndPoint localEndPoint)
+        :this(stream, localEndPoint, ServerCredentials.Insecure)
+    { }
+
+    public RemoteStreamServer(Stream stream, IPEndPoint localEndPoint, string cert, string key)
+        :this(stream, localEndPoint, BuildSslCredentials(cert, key))
+    { }
+
+    public RemoteStreamServer(Stream stream, IPEndPoint localEndPoint, ServerCredentials credentials)
     {
         _rpcService = new RemoteStreamServerRpcImpl(stream);
-        _rpcService.StreamClosed += (_, _) => Dispose();
+        _rpcService.ExceptionThrew += (_, ex) => ExceptionThrew?.Invoke(this, ex);
+        _rpcService.StreamClosed += (_, _) => StreamClosed?.Invoke(this, EventArgs.Empty);
 
         _server = new Grpc.Core.Server()
         {
-            //todo: add secure
-            Ports = { new ServerPort(localEndPoint.Address.ToString(), localEndPoint.Port, ServerCredentials.Insecure) },
+            Ports = { new ServerPort(localEndPoint.Address.ToString(), localEndPoint.Port, credentials) },
             Services = { RemoteStreamRpcService.BindService(_rpcService) }
         };
         _server.Start();
+    }
+
+    private static SslServerCredentials BuildSslCredentials(string cert, string key)
+    {
+        var keyCertPair = new KeyCertificatePair(cert, key);
+        return new SslServerCredentials([keyCertPair]);
     }
 
     #region Dispose
