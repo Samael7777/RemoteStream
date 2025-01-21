28f52f60d09965d293f6e60976c614fa60576f22
Вадим Кутьин <vadim.kutyn@gmail.com>
2025-01-21 23:01:30 +0100

    Rename IStream to IStreamService

--- RemoteStream.Server/RemoteStreamServer.cs
+++ RemoteStream.Server/RemoteStreamServer.cs
@@ -1,74 +1,8 @@
-﻿using System.Net;
-using Grpc.Core;
-using RemoteStream.Protocol;
+﻿using RemoteStream.Protocol;
 
 namespace RemoteStream.Server;
 
-public class RemoteStreamServer : IDisposable
+public class RemoteStreamServer : IStreamService
 {
-    private readonly Grpc.Core.Server _server;
-    private readonly RemoteStreamServerRpcImpl _rpcService;
-
-    public event EventHandler<Exception>? ExceptionThrew;
-    public event EventHandler? StreamClosed;
     
-    public RemoteStreamServer(Stream stream, IPEndPoint localEndPoint)
-        :this(stream, localEndPoint, ServerCredentials.Insecure)
-    { }
-
-    public RemoteStreamServer(Stream stream, IPEndPoint localEndPoint, string cert, string key)
-        :this(stream, localEndPoint, BuildSslCredentials(cert, key))
-    { }
-
-    public RemoteStreamServer(Stream stream, IPEndPoint localEndPoint, ServerCredentials credentials)
-    {
-        _rpcService = new RemoteStreamServerRpcImpl(stream);
-        _rpcService.ExceptionThrew += (_, ex) => ExceptionThrew?.Invoke(this, ex);
-        _rpcService.StreamClosed += (_, _) => StreamClosed?.Invoke(this, EventArgs.Empty);
-
-        _server = new Grpc.Core.Server()
-        {
-            Ports = { new ServerPort(localEndPoint.Address.ToString(), localEndPoint.Port, credentials) },
-            Services = { RemoteStreamRpcService.BindService(_rpcService) }
-        };
-        _server.Start();
-    }
-
-    private static SslServerCredentials BuildSslCredentials(string cert, string key)
-    {
-        var keyCertPair = new KeyCertificatePair(cert, key);
-        return new SslServerCredentials([keyCertPair]);
-    }
-
-    #region Dispose
-
-    private bool _disposed;
-
-    ~RemoteStreamServer()
-    {
-        Dispose(false);
-    }
-
-    public void Dispose()
-    {
-        Dispose(true);
-        GC.SuppressFinalize(this);
-    }
-
-    protected virtual void Dispose(bool disposing)
-    {
-        if (_disposed) return;
-        if (disposing)
-        {
-            //dispose managed state (managed objects)
-            _server.KillAsync().Wait();
-            _rpcService.Dispose();
-        }
-        //free unmanaged resources (unmanaged objects) and override finalizer
-        //set large fields to null
-
-        _disposed = true;
-    }
-
-    #endregion
 }
