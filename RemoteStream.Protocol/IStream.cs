51662fd2d6a54324211e1fec40cd187709924978
Вадим Кутьин <vadim.kutyn@gmail.com>
2025-01-21 22:37:47 +0100

    Add new IStream interface, delete old files

--- RemoteStream.Protocol/IStream.cs
+++ RemoteStream.Protocol/IStream.cs
@@ -6,31 +6,32 @@ namespace RemoteStream.Protocol;
 public interface IStream
 {
     [OperationContract] 
-    Response<StreamInfo> GetStreamInfo();
+    Task<Response<StreamInfo>> GetStreamInfo();
 
     [OperationContract] 
-    Response<long> GetLength();
+    Task<Response<long>> GetLength();
 
     [OperationContract]
-    Response<Empty> SetLength(long length);
+    Task<Response<Empty>> SetLength(long length);
 
     [OperationContract]
-    Response<long> GetPosition();
+    Task<Response<long>> GetPosition();
 
     [OperationContract]
-    Response<Empty> SetPosition(long position);
+    Task<Response<Empty>> SetPosition(long position);
+
+    [OperationContract]
+    Task<Response<long>> Seek(long offset, SeekOrigin origin);
+
+    [OperationContract]
+    Task<Response<Empty>> Write(ReadOnlySpan<byte> data);
+
+    [OperationContract]
+    Task<Response<byte[]>> Read(int len);
+
+    [OperationContract]
+    Task<Response<Empty>> Flush();
+    
+    [OperationContract]
+    Task<Response<Empty>> Close();
 }
-
-//service RemoteStreamRpcService {
-//rpc GetStreamInfo(google.protobuf.Empty) returns (StreamInfo);
-//rpc GetLength(google.protobuf.Empty) returns (LengthResponse);
-//rpc SetLength(LengthRequest) returns (StatusResponse);
-//rpc GetPosition(google.protobuf.Empty) returns (PositionResponse);
-//rpc SetPosition(PositionRequest) returns (StatusResponse);
-
-//rpc Seek(SeekRequest) returns (PositionResponse);
-//rpc Write(WriteRequest) returns (StatusResponse);
-//rpc Read(ReadRequest) returns (ReadResponse);
-//rpc Flush(google.protobuf.Empty) returns (StatusResponse);
-//rpc Close(google.protobuf.Empty) returns (StatusResponse);
-//}
--- RemoteStream.Protocol/Proto_old/ExceptionEx.cs
+++
@@ -1,9 +0,0 @@
-﻿namespace RemoteStream.Protocol.Proto_old;
-
-public static class ExceptionEx
-{
-    public static string ToExceptionObjectJson(this Exception ex)
-    {
-        return new ExceptionObject(ex).ToJson();
-    }
-}
